c1f06730eb3c889bd207818067e965a1d373e5a7
FlowRays <1581561246@qq.com>
2023-05-14 11:38:46 +0800

    feat: component DropdownSelect (#12)

--- components/DropdownSelect.py
+++ components/DropdownSelect.py
@@ -1,4 +1,4 @@
-from .BaseComponent import BaseComponent, Template, register_component
+from .BaseComponent import BaseComponent, Template, register_component, FormValueError
 from .BaseComponent import ImmutableMultiDict
 
 # 添加装饰器使程序能加载并注册这个组件
@@ -9,9 +9,22 @@ class DropdownSelect(BaseComponent):
     """下拉选择框
     参考：https://getbootstrap.com/docs/5.2/forms/select/
     """
-    # TODO: 完善组件
-
-    template = Template("")
+    template = Template("""
+        <div class="row">
+            <div class="col-md-6 mb-3">
+                <label for="{{name}}">{{index}}. {{caption}}</label>
+                <select class="form-select" id="{{name}}" name="{{name}}" required>
+                    <option value="" selected disabled>{{placeholder}}</option>
+                    {% for option in options %}
+                        <option value="{{option[0]}}">{{option[1]}}</option>
+                    {% endfor %}
+                </select>
+                {% if desc %}
+                    <div id="{{name}}-help" class="form-text">{{desc}}</div>
+                {% endif %}
+            </div>
+        </div>
+    """)
 
     def render(self,
                index: int,
@@ -27,7 +40,7 @@ class DropdownSelect(BaseComponent):
             index (int): 问题序号
             name (str): 问题名称（存储时使用）
             caption (str): 问题题干
-            option (list[(key: str, value: str)]): 问题的选项
+            options (list[(key: str, value: str)]): 问题的选项
                 key (str): 该项的键（存入表单的值）
                 value (str): 该项的展示值（渲染时展示的值）
             placeholder (str, optional): 输入提示. Defaults to "".
@@ -37,9 +50,15 @@ class DropdownSelect(BaseComponent):
         Returns:
             str: 渲染好的问卷
         """
-
-    # ruff: noqa: F811
-    render = BaseComponent.render  # 编写时删去此行
+        return self.template.render(
+            index=index,
+            name=name,
+            caption=caption,
+            options=options,
+            placeholder=placeholder,
+            required='true' if required else 'false',
+            desc=desc,
+        )
 
     def parse(self,
               name: str,
@@ -47,6 +66,10 @@ class DropdownSelect(BaseComponent):
               qdata=None,
               datatype='str',
               validation=None):
-        # TODO
-        # ? 验证一下是否用户所选都在问题提供的选项内
-        return super().parse(name, formdata, qdata, datatype, validation)
+        # 验证是否用户所选都在问题提供的选项内
+        selected = formdata.get(name)
+        options = [option[0] for option in qdata['options']]
+        if selected not in options:
+            raise FormValueError("Invalid option selected.")
+
+        return selected
